a4f3e2e9dfdffdf30f6f116035c452c0a7904944
JinX_D☆R3X <165863288+sirexx13@users.noreply.github.com>
2026-01-15 12:35:16 -0600

    Update flasher.js

--- fjs/flasher.js
+++ fjs/flasher.js
@@ -1,12 +1,11 @@
-const { ESPLoader } = window;
-
-
 document.getElementById("flash").onclick = async () => {
   const port = await navigator.serial.requestPort();
   await port.open({ baudRate: 921600 });
 
-  const loader = new ESPLoader({
-    transport: port,
+  const transport = new esptool.Transport(port);
+
+  const loader = new esptool.ESPLoader({
+    transport,
     baudrate: 921600,
     terminal: {
       clean() {},
@@ -16,28 +15,23 @@ document.getElementById("flash").onclick = async () => {
 
   await loader.main();
 
+  console.log("Connected to ESP");
+
   const files = [
     {
       address: 0x1000,
-      data: await fetch("Firmware/bootloader.bin").then(r => r.arrayBuffer())
+      data: await fetch("firmware/bootloader.bin").then(r => r.arrayBuffer())
     },
     {
       address: 0x8000,
-      data: await fetch("Firmware/partition-table.bin").then(r => r.arrayBuffer())
+      data: await fetch("firmware/partition-table.bin").then(r => r.arrayBuffer())
     },
     {
       address: 0x10000,
-      data: await fetch("Firmware/blackwall_firmware.bin").then(r => r.arrayBuffer())
+      data: await fetch("firmware/blackwall_firmware.bin").then(r => r.arrayBuffer())
     }
   ];
 
   await loader.writeFlash(files);
   alert("Flash complete");
 };
-
-
-
-
-
-
-
