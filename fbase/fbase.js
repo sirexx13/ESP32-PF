50632a9ed412234554af4ae87e50cb59dd233c11
JinX_D☆R3X <165863288+sirexx13@users.noreply.github.com>
2026-01-15 11:24:04 -0600

    Add files via upload

---
+++ fbase/fbase.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDaPCz3SirY8BrlyVz2XU_co2-3crPcJPs",
+    authDomain: "paradoxflasher.firebaseapp.com",
+    projectId: "paradoxflasher",
+    storageBucket: "paradoxflasher.firebasestorage.app",
+    messagingSenderId: "110590766394",
+    appId: "1:110590766394:web:ed7be5a5d5dfc90e03f403"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
