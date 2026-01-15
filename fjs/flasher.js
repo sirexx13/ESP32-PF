c5f56826831fbf0173c1012ab6f4324714f73310
JinX_D☆R3X <165863288+sirexx13@users.noreply.github.com>
2026-01-15 13:11:31 -0600

    Update flasher.js

--- fjs/flasher.js
+++ fjs/flasher.js
@@ -9,11 +9,14 @@ document.getElementById("flash").onclick = async () => {
     const transport = new esptool.Transport(port);
 
     const loader = new esptool.ESPLoader({
-      transport: new esptool.Transport(port),
+      transport,
       baudrate: 921600,
-      terminal: console   // just pass the browser console object
+      terminal: {
+        write: console.log.bind(console),
+        writeLine: console.log.bind(console),
+        clean: () => {}
+      }
     });
-
 
     await loader.main();
     console.log("Connected to ESP");
@@ -21,7 +24,7 @@ document.getElementById("flash").onclick = async () => {
     const files = [
       { address: 0x1000, data: await fetch("../Firmware/bootloader.bin").then(r => r.arrayBuffer()) },
       { address: 0x8000, data: await fetch("../Firmware/partition-table.bin").then(r => r.arrayBuffer()) },
-      { address: 0x10000, data: await fetch("..Firmware/blackwall_firmware.bin").then(r => r.arrayBuffer()) }
+      { address: 0x10000, data: await fetch("../Firmware/blackwall_firmware.bin").then(r => r.arrayBuffer()) }
     ];
 
     await loader.writeFlash(files);
@@ -32,6 +35,7 @@ document.getElementById("flash").onclick = async () => {
     alert("Flashing failed. Check console.");
   }
 };
+
 
 
 
