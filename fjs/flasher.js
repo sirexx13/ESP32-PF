b4b1450a391fee625c769731d2a8d7338b2fdc48
JinX_D☆R3X <165863288+sirexx13@users.noreply.github.com>
2026-01-15 14:48:13 -0600

    Update flasher.js

--- fjs/flasher.js
+++ fjs/flasher.js
@@ -1,7 +1,7 @@
 import {
   ESPLoader,
   Transport
-} from "https://app.unpkg.com/esptool-js@0.5.7";
+} from "https://unpkg.com/esptool-js@0.5.7";
 
 const connectBtn = document.getElementById("connect");
 const flashBtn   = document.getElementById("flash");
@@ -66,6 +66,7 @@ async function fetchBin(path) {
   const res = await fetch(path);
   return new Uint8Array(await res.arrayBuffer());
 }
+
 
 
 
